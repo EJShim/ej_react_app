6130453e96c6b2fad53804002b4c3ac047346b3f
ejshim <soulrommel@gmail.com>
2017-08-06 17:22:02 +0900

    video update

--- server/routes/video.js
+++ server/routes/video.js
@@ -5,28 +5,34 @@ import Spooky from 'spooky';
 
 const router = express.Router();
 
-
-
-
 router.get('/', (req, res) => {
-    let url = "https://www.youtube.com/user/soulrommel/videos?shelf_id=0&sort=dd&view=0";
+    // let url = "https://www.youtube.com/user/soulrommel/videos?shelf_id=0&sort=dd&view=0";
+
+    let url = 'https://www.youtube.com/watch?v=xMREVx4XaCY&list=PLcX8WMFmN4WMMJDQKSLL0EOxZDx99NkjZ';
+    
     let list = [];
-
-
-
     // USE REQUEST
     // Get Video List of ID :Soulrommel' and send response
     request(url, function (error, response, html) {
         if (!error) {
             let $ = cheerio.load(html);
 
-            $('.yt-lockup-thumbnail>span>a').each(function () {
-                let addr = $(this).attr("href");
-                let key = addr.split("=")[1];
+            let elements = $('.yt-uix-scroller-scroll-unit')
+            console.log("number of elements : ", elements.length);
 
-                // list.push("http://youtube.com/" + $(this).attr('href'))
-                list.push("https://www.youtube.com/embed/" + key)
-            });
+            elements.each(function(){
+                let data = {};
+                
+                data.id = $(this).attr('data-video-id');
+                data.image = $(this).attr('data-thumbnail-url');
+                data.title = $(this).attr('data-video-title');
+
+                if(data.id === undefined){return;}
+                //Rserve only image path
+                data.image = data.image.split("?")[0];                            
+
+                list.push( data);
+            });            
             res.send(list);
         }
     });
--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Header, Embed } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+import { Container, Header, Card, Icon, Image, Modal, Embed } from 'semantic-ui-react'
 import axios from 'axios';
 
 
@@ -16,29 +17,55 @@ class Videos extends React.Component{
 
     componentDidMount(){       
         
+        
         var that = this;
 
+        //Use Axios
         axios.get("/api/video") 
-            .then(response => {                                
+            .then(response => {
                 that.setState({videoList : response.data});
             })
             .catch(error => {
                 console.log(error);
             })
+
+        //Use Youtube API
         
     }
 
 
-    render(){
-        const videoListItem = this.state.videoList.map((video) =>
-                <iframe width="300" height="215" src={video} frameborder="0" allowfullscreen></iframe>             
-        );
+    render(){        
+        const cardListItem = this.state.videoList.map((video)=>
+            <Modal trigger={
+                <Card
+                    image={video.image}
+                    header={video.title}
+                    meta='video'
+                    description='Youtube'     
+                />
+            }>
+                <Modal.Header>{video.title}</Modal.Header>
+                <Modal.Content children>
+                <Embed
+                    id= {video.id}
+                    placeholder={video.image}
+                    source='youtube'
+                />
+                <Modal.Description>
+                    <Header>{video.title}</Header>
+                </Modal.Description>
+                </Modal.Content>
+            </Modal>                  
+            
+        );        
 
 
         return(
             <Container fluid>
                 <Header as='h2'>Videos</Header>
-                {videoListItem}
+                 <Card.Group>
+                   {cardListItem}
+                </Card.Group>
             </Container>
         );
     }
